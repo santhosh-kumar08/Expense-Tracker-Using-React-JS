1b0dcc1510fc496138685e88cbfc42067ef95cba
santhosh-kumar08 <147322065+santhosh-kumar08@users.noreply.github.com>
2025-08-27 20:36:28 +0530

    Update Header.js

--- Expense Tracker/src/components/Header.js
+++ Expense Tracker/src/components/Header.js
@@ -1,10 +1,11 @@
 export default function Header() {
   return (
-    <header className="Appheader">
-      <h1>
+    <header className="Appheader card p-3 mb-3 shadow-sm">
+      <h1 className="text-center">
         <img src="icon.png" alt="Expense Tracker Icon" className="logo" />
         Expense Tracker
       </h1>
     </header>
   );
 }
+
